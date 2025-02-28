eca9023948ebe5e5145318c1ca87ad804932365b
bertil <bertilfrigaard@gmail.com>
2025-02-28 16:08:06 +0100

    children spawning

---
+++ levelHandler.js
@@ -0,0 +1,50 @@
+//Handles children and bonus object handling
+function childrenOverTime(time) {
+    return Math.floor(time*0.1+1)
+}
+
+function updateLevel(updateTime) {
+    let childrenAtTime = childrenOverTime(updateTime);
+    while(children.length < childrenAtTime) {
+        addChild(100)
+    }
+}
+
+function addChild(speed) {
+    speed = speed;
+    direction = 0;
+    startX = 0;
+    startY = 0;
+
+    randint = Math.floor(Math.random()*4)
+    switch(randint) {
+        case 0: //TOP
+            startX = Math.random()*1000;
+            startY = 0;
+            direction = Math.PI;
+            break
+           
+        case 1: //BOTTOM
+            startX = Math.random()*1000;
+            startY = 1000;
+            direction = 0;
+            break
+            
+        case 2: //LEFT
+            startX = 0;
+            startY = Math.random()*1000;
+            direction = Math.PI*1.5;
+            break
+            
+        case 3: //RIGHT
+            startX = 1000;
+            startY = Math.random()*1000;
+            direction = Math.PI*0.5;
+            break 
+    }
+
+    direction += Math.random()*Math.PI;
+    children.push(new Child(startX, startY, direction, speed))
+
+}
+
--- main.js
+++ main.js
@@ -1,53 +1,36 @@
+let FONT_PressStart2P;
+
 let player;
 let children;
 
-function setup() 
-{
+let time;
+let countdown;
+
+function preload() {
+    FONT_PressStart2P = loadFont("/assets/PressStart2P-Regular.ttf");
+}
+
+function setup() {
 	createCanvas(1000, 1000);
     children = [];
     player = new Player(400,400,Math.PI*0.5,1000);
+    time = 0;
+    countdown = 20;
 }
 
-function addChild() {
-    speed = 120;
-    direction = 0;
-    startX = 0;
-    startY = 0;
-
-    randint = Math.floor(Math.random()*4)
-    switch(randint) {
-        case 0: //TOP
-            startX = Math.random()*1000;
-            startY = 0;
-            direction = Math.PI;
-            break
-           
-        case 1: //BOTTOM
-            startX = Math.random()*1000;
-            startY = 1000;
-            direction = 0;
-            break
-            
-        case 2: //LEFT
-            startX = 0;
-            startY = Math.random()*1000;
-            direction = Math.PI*1.5;
-            break
-            
-        case 3: //RIGHT
-            startX = 1000;
-            startY = Math.random()*1000;
-            direction = Math.PI*0.5;
-            break 
-    }
-
-    direction += Math.random()*Math.PI;
-    children.push(new Child(startX, startY, direction, speed))
-
+function getScore() {
+    return time*100
 }
 
-function draw()
-{
+function drawUI() {
+    textFont(FONT_PressStart2P);
+    textSize(25)
+    fill("red")
+    text("SCORE: " + Math.floor(getScore()/10)*10, 10, 45)
+    text("BOOM:  "+ Math.round(countdown), 10, 80)
+}
+
+function draw() {
     let deltaTime = 0.016;
     //UPDATE
     player.update(deltaTime)
@@ -58,6 +41,10 @@ function draw()
             children.splice(i, 1);
         }
     }
+    updateLevel(time)
+
+    time += deltaTime
+    countdown -= deltaTime
 
     //DRAW
     background(220,220,220)
@@ -65,4 +52,5 @@ function draw()
     for(let i = 0; i < children.length; i++) {
         children[i].draw()
     }
+    drawUI()
 }
