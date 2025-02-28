b3996bdbf6c4a0dce86a8d29da481d2c77313c6b
bertil <bertilfrigaard@gmail.com>
2025-02-28 12:57:30 +0100

    Make player stay in bound

--- player_class.js
+++ player_class.js
@@ -1,5 +1,9 @@
 class Player extends MovingObject
 {
+    constructor(x, y, direction, speed) {
+        super(x, y, direction, speed)
+        this.diameter = 50;
+    }
     draw(){
         fill('Blue');
         circle(this.x,this.y,50);
@@ -7,6 +11,25 @@ class Player extends MovingObject
     update(deltaTime) {
         super.update(deltaTime);
         this.changeDirection(deltaTime);
+        this.resolveOutOfBounds();
+    }
+    resolveOutOfBounds() {
+        if(this.x > 1000 - this.diameter/2) {
+            //Out of bonds RIGHT
+            this.x = 1000 - this.diameter/2
+        }
+        if(this.x < 0 + this.diameter/2) {
+            //Out of bonds LEFT
+            this.x = 0 + this.diameter/2
+        }
+        if(this.y > 1000 - this.diameter/2) {
+            //Out of bonds BOTTOM
+            this.y = 1000 - this.diameter/2;
+        }
+        if(this.y < 0 + this.diameter/2) {
+            //Out of bonds TOP
+            this.y = 0 + this.diameter/2
+        }
     }
     changeDirection(deltaTime) {
         let change = 5;
